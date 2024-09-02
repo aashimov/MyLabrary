88c91a9bff8188e3491377a16f1fa4146251eb1f
Adilbek Ashimov <aashimov@dc.devcats.kg>
2024-09-02 18:14:28 +0600

    initial commit

---
+++ my-custom-view-library/src/main/java/com/example/my_custom_view_library/Math.kt
@@ -0,0 +1,16 @@
+package com.example.my_custom_view_library
+
+class Math {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+}
